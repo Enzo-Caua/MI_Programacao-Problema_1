20a37b30e95747a8d8fd2b4f76a69aec85f0a885
Enzo-Caua <caua7uefs@gmail.com>
2025-08-15 22:53:43 -0300

    Delete JavaDoc directory

--- JavaDoc/JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Avaliacao"},{"l":"Busca"},{"l":"Cadastro"},{"l":"Listagem"},{"l":"Main"},{"l":"Menus"},{"l":"Obras"},{"l":"Testes.Integracao"},{"l":"Testes.Unitarios"}];updateSearchResults();
